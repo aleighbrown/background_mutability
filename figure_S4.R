fc90df31ad86c966ecc8baced59b01d5f1de9e74
AL Brown <brown.annaleigh@gmail.com>
2019-02-19 15:30:20 -0500

    Minor updates

--- figure_S4.R
+++ figure_S4.R
@@ -1,114 +1,53 @@
 
-#read in the brca1
-if(!exists("allBRCA1")){
-  allBRCA1 <- fread("~/mahmood/binom/analyzed data/analyzed_allBRCA1diffmodels.csv")
-}
+full_combined_dataset <- fread("~/mahmood/binom/analyzed data/final_benchmark.csv")
+full_combined_dataset[,MSKBScore := binom.test(x = (MSKFreq + 1), p = Mutability, n = 9228, alt = "g")$p.value, by = 1:nrow(full_combined_dataset)]
 
-#read in the TP53
-if(!exists("allTP53")){
-  allTP53 <- fread("~/mahmood/binom/analyzed data/analyzed_allTP53diffmodels.csv")
-}
-allTP53[CancerModel == "Lung Cancer", CancerModel := "Lung Adenocarcinoma"]
-#read in the TP53
-if(!exists("allMart")){
-  allMart <- fread("~/mahmood/binom/analyzed data/analyzed_allMartelottodiffmodels.csv")
-}
-
-allBRCA1$label <- factor(allBRCA1$label, levels = c("Neutral","Deleterious"))
+#setnames(full_combined_dataset, c("fathmScore","CountNew","binomial","CSB","mutability", "vest","candra_score"), c("FatHMM","Frequency","B-Score","CHASM","Mutability", "VEST", "CanDrA"))
 
 
-all_B1 <- ggplot(allBRCA1, aes(label, log10(mutability), fill = label)) + 
-  geom_boxplot() + facet_grid(. ~ CancerModel) + scale_fill_manual(values = c("#ffeda0","#f03b20")) + theme(axis.title = element_blank()) +
-  theme(strip.text= element_blank(),axis.title = element_blank(), axis.text.x = element_blank()) +  scale_y_continuous(breaks=c(-7, -6,-5,-4),labels = c("7","6","5","4")) +
-  theme(legend.position = "none" ) +
-  theme(plot.margin=unit(c(0,0,0,1.2),"cm"))
+scores =  c("FatHMM","Frequency","B-Score","CHASMplus", "VEST", "CanDrA","REVEL","CHASM","Mutability")
+
+negsvec =  get_negs_vector(full_combined_dataset, scores)
+
+combined_roc_table <- make_roc_table(full_combined_dataset,scores = scores, negs = negsvec)
+ 
+combined_roc_table <- combined_roc_table[!(Measure == "Frequency" & alpha == 0)]
+
+combined_pr_table <- make_pr_table(full_combined_dataset,scores = scores ,negs = negsvec)
+
+combined_pr_table <- combined_pr_table[!(Measure == "Frequency" & alpha == 0)]
+
+one <- plot_roc_table(combined_roc_table)
+one_withinset <- make_inset_ROC(one,xmin = 0.3,ymax = 0.55,ymin = -0.07) + theme(legend.position = c(0.7,0.7))
+ 
+one_withinset <- annotate_figure(one_withinset,bottom = text_grob("False positive rate", face = "bold", size = 18), left = text_grob("True positive rate", rot = 90,face = "bold", size = 18))
+
+two <- plot_pr_table(combined_pr_table) + theme(legend.position = "none")
+#two_withinset <- make_inset_ROC(two,zoomXmin = 0,zoomXmax = 0.5, zoomYmin = 0.5, zoomYmax = 1,ab = F,xbreaks = c(0,0.25,0.5), xmin = 0.2,xmax = 0.76,ymin = -0.05,ybreaks = c(0.5, 0.66,.85,1))
+two_withinset <- annotate_figure(two,bottom = text_grob("Recall", face = "bold", size = 18), left = text_grob("Precision", rot = 90,face = "bold", size = 18))
+both <- ggarrange(one_withinset, two_withinset, labels = c("A","B"))
+both
+
+make_score_table(full_combined_dataset,scores = scores, negs = negsvec, roundto = 2)
+
+scores_msk =  c("FatHMM","MSKFreq","MSKBScore","CHASMplus","Mutability", "VEST", "CanDrA","REVEL","CHASM")
+negsvecmsk =  get_negs_vector(full_combined_dataset, scores_msk)
+
+make_score_table(full_combined_dataset[Frequency != 0],scores = scores, negs = negsvec, roundto = 2)
+
+make_score_table(full_combined_dataset[MSKFreq != 0],scores = scores_msk, negs = negsvecmsk, roundto = 4)
 
 
-allMart[,label := as.character(label)]
-allMart$label <- factor(allMart$label, levels = c("0","1"))
 
+###Rare
+scores_rare =  c("FatHMM","B-Score","CHASMplus", "VEST", "CanDrA","REVEL","CHASM")
+scores_raremsk =  c("FatHMM","MSKBScore","CHASMplus", "VEST", "CanDrA","REVEL","CHASM")
 
-allMart_plot <- ggplot(allMart, aes(label, log10(mutability), fill = label)) + 
-  geom_boxplot() + facet_grid(. ~ CancerModel) + scale_fill_manual(values = c("#ffeda0","#f03b20")) + theme(axis.title = element_blank()) +
-  theme(strip.text= element_blank(),axis.title = element_blank(), axis.text.x = element_blank()) +  scale_y_continuous(breaks=c(-7, -6,-5,-4),labels = c("7","6","5","4")) +
-  theme(legend.position = "none" ) +
-  theme(plot.margin=unit(c(0,0,0,1.2),"cm"))
+negsvec_rare =  get_negs_vector(full_combined_dataset, scores_rare)
+negsvec_raremsk =  get_negs_vector(full_combined_dataset, scores_raremsk)
 
+make_score_table(full_combined_dataset[Frequency == 0],scores = scores_rare, negs = negsvec_rare)
+make_score_table(full_combined_dataset[Frequency == 1],scores = scores_rare, negs = negsvec_rare)
 
-
-
-
-
-allTP53$label <- factor(allTP53$label, levels = c("functional","partially-functional", "non-functional"))
-
-
-all_T <- ggplot(allTP53, aes(label, log10(mutability), fill = label)) + 
-  geom_boxplot() + facet_grid(. ~CancerModel) + scale_fill_brewer(palette = "YlOrRd",labels = c("Functional","Partially Functional", "Damaging")) + theme(axis.title = element_blank()) +
-  theme(axis.title = element_blank(), axis.text.x = element_blank()) + 
-  scale_y_continuous(breaks=c(-7, -6,-5, -4),labels = c("7","6","5","4")) +
-  theme(legend.position = "none") + 
-  scale_x_discrete(labels = c("Functional","Partially-Functional", "Damaging")) +
-  theme(strip.text= element_text(size = 9))
-  theme(plot.margin=unit(c(0,0,0,1.2),"cm"))
-  
-
-fig6 <- ggarrange(all_T, all_B1,allMart_plot, nrow = 3, labels = c("A","B","C"),align = "hv")
-
-fig6 <- annotate_figure(fig6,left = text_grob(expression(bold(paste("-Log"[10], "Mutability"))),rot = 90))
-fig6
-
-
-breast<- readPNG("~/Desktop/9032.png")
-lung <- readPNG("~/Desktop/9026.png")
-pancan <- readPNG("~/Desktop/33999.png")
-skin <- readPNG("~/Desktop/9025.png")
-snp <- readPNG("~/Desktop/34275.png")
-
-rH = 0.095
-rW = 0.15 
-disp = 0.18
-first = -.31
-picy = .435
-
-ggdraw() + draw_plot(fig6,0, 0, 1, .90) + 
-  draw_plot(rasterGrob(breast,height = rH,width = rW),x = first, y = picy) +
-  draw_plot(rasterGrob(lung,height = rH,width = rW),x = first + disp, y = picy) +
-  draw_plot(rasterGrob(skin,height = rH,width = rW),x = first + 2*disp, y = picy) +
-  draw_plot(rasterGrob(pancan,height = rH,width = rW),x = first + 3*disp, y = picy) +
-  draw_plot(rasterGrob(snp,height = rH,width = rW),x = first + 4*disp, y = picy) 
-
-
-brca_p <- data.table()  
-for(m in unique(allBRCA1[,CancerModel])){
-  print(m)
-  test <- (wilcox.test(allBRCA1[label == "Neutral" & CancerModel == m, mutability],allBRCA1[label != "Neutral" & CancerModel == m, mutability]))
-  brca_p <- rbind(brca_p, cbind(m,test$p.value,test$statistic))
-  
-}
-
-names(brca_p) <- c("Model", "Pvalue", "W Statistic")
-
-p53_p <- data.table()  
-
-for(m in unique(allTP53[,CancerModel])){
-  print(m)
-  test <- dunn.test(allTP53[CancerModel == m, mutability],allTP53[CancerModel == m, label],method = "by")
-  print(test)
-  p53_p <- rbind(p53_p, cbind(as.data.table(test),rep(m,3)),fill = T)
-}
-
-
-martelotto_p <- data.table()  
-for(m in unique(allMart[,CancerModel])){
-  print(m)
-  test <- (wilcox.test(allMart[label == "0" & CancerModel == m, mutability],allMart[label == "1" & CancerModel == m, mutability]))
-  martelotto_p <- rbind(martelotto_p, cbind(m,test$p.value,test$statistic))
-  
-}
-
-names(martelotto_p) <- c("Model", "Pvalue", "W Statistic")
-
-
-fwrite(p53_p, "~/mahmood/binom/analyzed data/TP53 statistical comparisons Diff Background.csv")
-fwrite(brca_p, "~/mahmood/binom/analyzed data/BRCA1 statistical comparisons Diff Background.csv")
-fwrite(martelotto_p, "~/mahmood/binom/analyzed data/Martelotto statistical comparisons Diff Background.csv")
+make_score_table(full_combined_dataset[MSKFreq == 0],scores = scores_raremsk, negs = negsvec_raremsk)
+make_score_table(full_combined_dataset[MSKFreq == 1],scores = scores_raremsk, negs = negsvec_raremsk)
--- figure_S5.R
+++ figure_S5.R
@@ -4,7 +4,8 @@ full_combined_dataset[,MSKBScore := binom.test(x = (MSKFreq + 1), p = Mutability
 
 #setnames(full_combined_dataset, c("fathmScore","CountNew","binomial","CSB","mutability", "vest","candra_score"), c("FatHMM","Frequency","B-Score","CHASM","Mutability", "VEST", "CanDrA"))
 
-scores =  c("FatHMM","Frequency","B-Score","CHASMplus", "VEST", "CanDrA","REVEL")
+
+scores =  c("FatHMM","Frequency","B-Score","CHASMplus", "VEST", "CanDrA","REVEL","CHASM","Mutability")
 
 negsvec =  get_negs_vector(full_combined_dataset, scores)
 
@@ -17,34 +18,36 @@ combined_pr_table <- make_pr_table(full_combined_dataset,scores = scores ,negs =
 combined_pr_table <- combined_pr_table[!(Measure == "Frequency" & alpha == 0)]
 
 one <- plot_roc_table(combined_roc_table)
-one_withinset <- make_inset_ROC(one,xmin = 0.3,ymax = 0.65) + theme(legend.position = c(0.7,0.7))
+one_withinset <- make_inset_ROC(one,xmin = 0.3,ymax = 0.55,ymin = -0.07) + theme(legend.position = c(0.7,0.7))
  
-one_withinset <- annotate_figure(one_withinset,bottom = text_grob("False positive rate", face = "bold"), left = text_grob("True positive rate", rot = 90,face = "bold"))
+one_withinset <- annotate_figure(one_withinset,bottom = text_grob("False positive rate", face = "bold", size = 18), left = text_grob("True positive rate", rot = 90,face = "bold", size = 18))
 
 two <- plot_pr_table(combined_pr_table) + theme(legend.position = "none")
 #two_withinset <- make_inset_ROC(two,zoomXmin = 0,zoomXmax = 0.5, zoomYmin = 0.5, zoomYmax = 1,ab = F,xbreaks = c(0,0.25,0.5), xmin = 0.2,xmax = 0.76,ymin = -0.05,ybreaks = c(0.5, 0.66,.85,1))
-two_withinset <- annotate_figure(two,bottom = text_grob("Recall", face = "bold"), left = text_grob("Precision", rot = 90,face = "bold"))
+two_withinset <- annotate_figure(two,bottom = text_grob("Recall", face = "bold", size = 18), left = text_grob("Precision", rot = 90,face = "bold", size = 18))
 both <- ggarrange(one_withinset, two_withinset, labels = c("A","B"))
 both
 
+make_score_table(full_combined_dataset,scores = scores, negs = negsvec, roundto = 2)
 
-scores_msk =  c("FatHMM","MSKFreq","MSKBScore","CHASMplus","Mutability", "VEST", "CanDrA","REVEL")
+scores_msk =  c("FatHMM","MSKFreq","MSKBScore","CHASMplus","Mutability", "VEST", "CanDrA","REVEL","CHASM")
+negsvecmsk =  get_negs_vector(full_combined_dataset, scores_msk)
 
+make_score_table(full_combined_dataset[Frequency != 0],scores = scores, negs = negsvec, roundto = 2)
 
-make_score_table(full_combined_dataset[Frequency != 0],scores = scores, negs = negsvec)
-
-make_score_table(full_combined_dataset[MSKFreq != 0],scores = scores_msk, negs = negsvec)
+make_score_table(full_combined_dataset[MSKFreq != 0],scores = scores_msk, negs = negsvecmsk, roundto = 4)
 
 
 
 ###Rare
-scores_rare =  c("FatHMM","B-Score","CHASMplus", "VEST", "CanDrA","REVEL")
-scores_raremsk =  c("FatHMM","MSKBScore","CHASMplus", "VEST", "CanDrA","REVEL")
+scores_rare =  c("FatHMM","B-Score","CHASMplus", "VEST", "CanDrA","REVEL","CHASM")
+scores_raremsk =  c("FatHMM","MSKBScore","CHASMplus", "VEST", "CanDrA","REVEL","CHASM")
 
 negsvec_rare =  get_negs_vector(full_combined_dataset, scores_rare)
+negsvec_raremsk =  get_negs_vector(full_combined_dataset, scores_raremsk)
 
 make_score_table(full_combined_dataset[Frequency == 0],scores = scores_rare, negs = negsvec_rare)
 make_score_table(full_combined_dataset[Frequency == 1],scores = scores_rare, negs = negsvec_rare)
 
-make_score_table(full_combined_dataset[MSKFreq == 0],scores = scores_raremsk, negs = negsvec_rare)
-make_score_table(full_combined_dataset[MSKFreq == 1],scores = scores_raremsk, negs = negsvec_rare)
+make_score_table(full_combined_dataset[MSKFreq == 0],scores = scores_raremsk, negs = negsvec_raremsk)
+make_score_table(full_combined_dataset[MSKFreq == 1],scores = scores_raremsk, negs = negsvec_raremsk)
